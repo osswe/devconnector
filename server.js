52ca448e2d70f83809edafbccbe17de080228531
mmccue <mmccue@paylocity.com>
2018-09-07 20:43:42 -0500

    adding mongoose

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+const db = require('./config/keys').mongoURI;
+
+// connect to mongo db through mongoose
+mongoose
+  .connect(db)
+  .then(() => { console.log('mongo db connected....') })
+  .catch((err) => { console.log('error connecting to mongo:', err) });
 
 app.get('/', (req, res) => res.send('hello!'));
 
