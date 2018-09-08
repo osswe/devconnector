787df4c28c1c84d00eb5b859baa3d5ce7000c146
mmccue <mmccue@paylocity.com>
2018-09-07 21:00:15 -0500

    missed one

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 //this url is relative to the route that was defined to get here, i.e. the app use having '/api/users' to make
 // this full url /api/users/test
+
+// @route GET api/profile/test
+// @desc Tests profile route
+// @access Public
+
 router.get('/test', (req, res) => res.json({ msg: "Profile works" }));
 
 module.exports = router;
